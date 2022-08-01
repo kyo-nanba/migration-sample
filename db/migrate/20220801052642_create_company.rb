60cc7b9758022c5f98001b8376dfd224f3d02e26
Kyohei Nanba <1596772+kyo-nanba@users.noreply.github.com>
2022-08-01 14:27:06 +0900

    create companies table

---
+++ db/migrate/20220801052642_create_company.rb
@@ -0,0 +1,8 @@
+class CreateCompany < ActiveRecord::Migration[7.0]
+  def change
+    create_table :companies do |t|
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_01_045409) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_01_052642) do
+  create_table "companies", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tasks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
